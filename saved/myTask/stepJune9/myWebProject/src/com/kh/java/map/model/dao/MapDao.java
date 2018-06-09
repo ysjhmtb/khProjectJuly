892f8a4581860bfe6259cecb31dd1ce457f9c625
YSJ <ysjhmtb@gmail.com>
2018-06-09 23:29:45 +0900

    Implementing Model and Controller
    ongoing Model and Controller in MVC.
    It is required unit test, in order that code quality and stability is guaranted by TDD.

--- saved/myTask/stepJune9/myWebProject/src/com/kh/java/map/model/dao/MapDao.java
+++ saved/myTask/stepJune9/myWebProject/src/com/kh/java/map/model/dao/MapDao.java
@@ -2,9 +2,11 @@ package com.kh.java.map.model.dao;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import com.kh.java.common.JDBCTemplate;
 import com.kh.java.map.model.vo.MapVo;
 
 public class MapDao {
@@ -13,7 +15,33 @@ public class MapDao {
 		
 		Statement stmt = null;
 		ResultSet rs = null;
+		ArrayList<MapVo> result = new ArrayList<>();
 		
+		try {
+			stmt = con.createStatement();
+			String query = "SELECT MARNAME, MARNO, LAT, LNG, EXPL FROM TABLE MARLOC";
+			rs = stmt.executeQuery(query);
+			
+			MapVo temp = null;
+			while(rs.next()) {
+				String marketName = rs.getString("MARNAME");
+				int marketNo = rs.getInt("MARNO");
+				double marketLat = rs.getDouble("LAT");
+				double marketLng = rs.getDouble("LNG");
+				String marketExpl = rs.getString("EXPL");
+				
+				temp = new MapVo(marketName, marketNo, marketLat, marketLng, marketExpl);
+				result.add(temp);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JDBCTemplate.close(rs);
+			JDBCTemplate.close(stmt);
+		}
 		
+		return result;
 	}
 }
--- saved/myTask/stepJune9/myWebProject/src/com/kh/java/map/model/service/MapService.java
+++ saved/myTask/stepJune9/myWebProject/src/com/kh/java/map/model/service/MapService.java
@@ -1,5 +1,19 @@
 package com.kh.java.map.model.service;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.kh.java.common.JDBCTemplate;
+import com.kh.java.map.model.dao.MapDao;
+import com.kh.java.map.model.vo.MapVo;
+
 public class MapService {
 
+	public List<MapVo> getMapList(){
+		Connection con = JDBCTemplate.getConnection();
+		ArrayList<MapVo> list = new MapDao().selectMapList(con);
+		JDBCTemplate.close(con);
+		return list;
+	}
 }
