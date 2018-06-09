0fd71ed01cab11f841a53df59b43dc357795d18a
YSJ <ysjhmtb@gmail.com>
2018-06-09 14:39:17 +0900

    JDBCTemplate의 getConnection 작성과 설정
    DB에 접속하기 위한 정보를 properties 파일로 분리하고, 이에 대한 설정 완료.
    
    이런 설정 정보를 바탕으로 getConnection 메소드 작성.

--- saved/myTask/stepJune9/myWebProject/src/com/kh/java/common/JDBCTemplate.java
+++ saved/myTask/stepJune9/myWebProject/src/com/kh/java/common/JDBCTemplate.java
@@ -1,10 +1,44 @@
 package com.kh.java.common;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class JDBCTemplate {
 
 	public static Connection getConnection() {
+		Connection con = null;
 		
+		try {
+			String filePath = 
+					JDBCTemplate.class.getResource("/oracleDriver.properties").getPath();
+			Properties prop = new Properties();
+			prop.load(new FileReader(filePath));
+			
+			String driver  = prop.getProperty("driver");
+			String url = prop.getProperty("url");
+			String id = prop.getProperty("id");
+			String pwd = prop.getProperty("pwd");
+			
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, id, pwd);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return con;
 	}
+	
+	
 }
